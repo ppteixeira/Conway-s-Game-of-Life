7cc59239bb598fa12dab1cf5f61b7b5c7be1d82b
Pedro Araujo <ppteixeira@outlook.com>
2017-03-07 01:31:26 +1100

    Fix find_neighbours,going through. But still seems not right

--- app/board.rb
+++ app/board.rb
@@ -4,12 +4,12 @@ require 'io/console'
 class Board
 
   def initialize(collumns = 80, rows = 20)
-    @board = create_board(collumns, rows)
+    @board = create_board(rows, collumns)
   end
 
-  def create_board(collumns, rows)
-    (1..rows).collect do |row|
-      (1..collumns).collect do |collumn|
+  def create_board(rows, collumns)
+    (0...rows).collect do |row|
+      (0...collumns).collect do |collumn|
         Cell.new(row, collumn)
       end
     end
@@ -18,6 +18,7 @@ class Board
   def start_game
     loop do
       show_board
+      look_neighbours
       binding.pry
       play_action
       sleep(0.5)
@@ -30,7 +31,14 @@ class Board
       puts ''
       row.each do |element|
         print element.result
-        element.find_neighbours(@board)
+      end
+    end
+  end
+
+  def look_neighbours
+    @board.each do |row|
+      row.each do |element|
+         element.find_neighbours(@board)
       end
     end
   end
@@ -39,10 +47,9 @@ class Board
     @board.each do |row|
       row.each do |element|
         element.play_move
-      end;
+      end
     end
   end
 end
-binding.pry
 b = Board.new
 b.start_game
--- app/cell.rb
+++ app/cell.rb
@@ -1,15 +1,15 @@
 class Cell
-  attr_accessor :cell_x, :cell_y, :neighbours_alive
+  attr_accessor :cell_col, :cell_row, :neighbours_alive
 
-  def initialize(x, y)
-    @cell_x = x
-    @cell_y = y
+  def initialize(row, collumn)
+    @cell_col = collumn
+    @cell_row = row
     @state = ['ON','OFF','OFF','OFF'].sample
     @neighbours_alive = []
   end
 
   def result
-    is_on? ? "\u25A3" : "\s"
+    is_on? ? "O" : "\s"
   end
 
   def is_on?
@@ -26,11 +26,13 @@ class Cell
 
   def find_neighbours(board)
     neighbours_alive.clear
-    (cell_y-1..cell_y+1).each do |row|
-      if row > 0 && row <= 20
-        (cell_x-1..cell_x+1).each do |collumn|
-          if collumn > 0 && collumn <= 80
-            if row != cell_x && collumn != cell_y
+    (cell_row-1..cell_row+1).each do |row|
+      if row >= 0 && row < 20
+        (cell_col-1..cell_col+1).each do |collumn|
+          if collumn >= 0 && collumn < 80
+            if row != cell_row && collumn != cell_col
+              if board[row][collumn] == nil
+              end
               neighbours_alive << board[row][collumn] if board[row][collumn].is_on?
             end
           end
@@ -41,6 +43,7 @@ class Cell
   end
 
   def play_move
+    binding.pry
     case is_on?
     when true
       turn_off if neighbours_alive.size < 2 || neighbours_alive.size > 3
